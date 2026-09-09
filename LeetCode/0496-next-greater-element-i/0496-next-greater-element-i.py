69ef93750021fb50672922747c0f724231abbcc2
윤종휘 <58451748+Jongeume@users.noreply.github.com>
2026-09-09 19:26:58 +0900

    .py / Easy /0496-next-greater-element-i

--- LeetCode/0496-next-greater-element-i/0496-next-greater-element-i.py
+++ LeetCode/0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -6,18 +6,9 @@ class Solution:
         for i in nums2: 
             if not stack:
                 stack.append(i)
-                
-            if stack[-1] < i:
-                while stack:
-                    if stack[-1] < i: 
-                        result[stack.pop()] = i
-                    else :
-                        break
+            while stack and stack[-1] < i :
+                result[stack.pop()] = i
             stack.append(i)
-            
-        # for j in range(len(nums1)):
-        #     nums1[j] = result[nums1[j]]
-
         return [result[n] for n in nums1]
 
 
