0b8fb9cc6b7a1b064c70c0af0f50b52856790728
윤종휘 <58451748+Jongeume@users.noreply.github.com>
2026-09-15 11:18:27 +0900

    Math /.py / Easy /0070-climbing-stairs

---
+++ LeetCode/0070-climbing-stairs/0070-climbing-stairs.py
@@ -0,0 +1,27 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        # 첫번째 그냥 재귀 방식
+        # # 한번에 1 or 2칸 씩 오를 수 있다.
+        # # n-1, n-2
+
+        # # base case
+        # if n==1:
+        #     return 1
+        # if n==2:
+        #     return 2
+
+        # return self.climbStairs(n-1) + self.climbStairs(n-2)
+
+        # 두번째 메모이제이션
+        # 딕셔너리로 값 저장
+        memo = {}
+
+        def f(n):
+            if n <= 2:
+                return n
+            if n in memo:
+                return memo[n]
+            memo[n] = f(n - 1) + f(n - 2)
+            return memo[n]
+
+        return f(n)
